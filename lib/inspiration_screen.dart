e6c1b9b41f3f314f4a7343dc08041bcf7f9cfe73
nachoggodino <nachogonzalezgodino@gmail.com>
2023-10-17 23:28:05 +0200

    Añado fecha y randomizo la foto para el día del año

--- lib/inspiration_screen.dart
+++ lib/inspiration_screen.dart
@@ -4,6 +4,7 @@ import 'package:al_moon/inspiration.dart';
 import 'package:al_moon/inspiration_data.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:jiffy/jiffy.dart';
 
 final _random = Random();
 
@@ -15,60 +16,99 @@ class InspirationScreen extends StatefulWidget {
 }
 
 class _InspirationScreenState extends State<InspirationScreen> {
-  List<Inspiration> _inspirations = List.from(inspirations);
+  Inspiration _inspiration = inspirations[Jiffy.now().dayOfYear % inspirations.length];
 
   @override
   Widget build(BuildContext context) {
-    if (_inspirations.isEmpty) {
-      _inspirations = List.from(inspirations);
-    }
-    final Inspiration inspiration = _inspirations[_random.nextInt(_inspirations.length)];
     return Scaffold(
-      extendBodyBehindAppBar: true,
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Theme.of(context).colorScheme.primary,
-        onPressed: () {
-          _inspirations.remove(inspiration);
-          setState(() {});
-        },
-        child: const Icon(
-          Icons.shuffle,
-        ),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-      body: Container(
-        width: double.infinity,
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage(inspiration.image),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: Container(
-          decoration: BoxDecoration(
-            
-            gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: [
-                  Colors.transparent,
-                  Theme.of(context).colorScheme.background,
-                ]),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 70, horizontal: 30),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Text(
-                  textAlign: TextAlign.center,
-                  inspiration.text,
-                  style: GoogleFonts.zenLoop(fontSize: 40, color: Colors.white),
+      body: Stack(
+        children: [
+          Container(
+            width: double.infinity,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(_inspiration.image),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      Colors.transparent,
+                      Theme.of(context).colorScheme.background,
+                    ]),
+              ),
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 70, horizontal: 30),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      textAlign: TextAlign.center,
+                      _inspiration.text,
+                      style: GoogleFonts.zenLoop(
+                          fontSize: 40, color: Colors.white),
+                    ),
+                  ],
                 ),
-              ],
+              ),
             ),
           ),
-        ),
+          Align(
+            alignment: Alignment.topRight,
+            child: Container(
+              margin: const EdgeInsets.symmetric(horizontal: 4, vertical: 56),
+              child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    shape: const CircleBorder(),
+                    padding: const EdgeInsets.all(6),
+                    backgroundColor: Theme.of(context).colorScheme.primary,
+                    foregroundColor: const Color.fromARGB(255, 44, 44, 44),
+                  ),
+                  child: const Icon(
+                    Icons.collections_outlined,
+                  )),
+            ),
+          ),
+          Align(
+            alignment: Alignment.topRight,
+            child: Container(
+              margin: const EdgeInsets.symmetric(horizontal: 4, vertical: 112),
+              child: ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    _inspiration =
+                        inspirations[_random.nextInt(inspirations.length)];
+                  });
+                },
+                style: ElevatedButton.styleFrom(
+                  shape: const CircleBorder(),
+                  padding: const EdgeInsets.all(6),
+                  backgroundColor: Theme.of(context).colorScheme.primary,
+                  foregroundColor: const Color.fromARGB(255, 44, 44, 44),
+                ),
+                child: const Icon(
+                  Icons.all_inclusive_rounded,
+                ),
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Container(
+              margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+              child: Text(
+                Jiffy.now().yMMMMd,
+                style: GoogleFonts.zenLoop(fontSize: 16, color: Colors.white),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'inspiration_screen.dart'; // Importa el componente InspirationScreen
 
 void main() {
